565adb684ad8b2ada75f24e230a5b9d7a1f900eb
Intengine <studio@intengine.pl>
2019-04-10 11:37:00 +0200

    Added Player Controller
    Added Animator, started to code some things.

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Animator playerAnimator;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}

7aa6804f36731094cc89fa5c6e6e2580a5a922d5
Intengine <studio@intengine.pl>
2019-04-15 12:22:54 +0200

    Added mouse pointer particles
    It left them on the stage, have to fix the issue.

--- Assets/Scripts/Camera Scripts/MouseScript.cs
+++ Assets/Scripts/Camera Scripts/MouseScript.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class MouseScript : MonoBehaviour
 {
     public Texture2D cursorTexture;
-    public GameObject mousePoint;
     private CursorMode mode = CursorMode.ForceSoftware;
     private Vector2 hotSpot = Vector2.zero;
+
+    public GameObject mousePoint;
+    private GameObject instantiatedMouse;
 
     void Start()
     {
@@ -17,5 +19,22 @@ public class MouseScript : MonoBehaviour
     void Update()
     {
         Cursor.SetCursor(cursorTexture, hotSpot, mode);
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider is TerrainCollider)
+                {
+                    Vector3 temporaryPosition = hit.point;
+                    temporaryPosition.y = 0.35f;
+
+                    Instantiate(mousePoint, temporaryPosition, Quaternion.identity);
+                }
+            }
+        }
     }
 }

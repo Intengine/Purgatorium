279029667537448e05a54dc5a2af0630434bdbf5
Intengine <studio@intengine.pl>
2019-04-16 11:25:31 +0200

    Started to add some attack effects
    Working fine, but one power at a time. Need to fix it plus add more effects.

---
+++ Assets/Scripts/Player Scripts/PlayerAttackEffects.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackEffects : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void GroundImpact()
+    {
+
+    }
+
+    void Kick()
+    {
+
+    }
+
+    void FireTornado()
+    {
+
+    }
+
+    void FireShield()
+    {
+
+    }
+
+    void Heal()
+    {
+
+    }
+
+    void ThunderAttack()
+    {
+
+    }
+}

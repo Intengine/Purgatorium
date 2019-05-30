90ad243bcf99713865d6078832a0885e40e0df95
Intengine <studio@intengine.pl>
2019-05-30 19:24:10 +0200

    Started to add some patrol points
    Choose another way for controlling AI.

---
+++ Assets/Scripts/Enemy Scripts/EnemyPatrol.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform[] walkPoints;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

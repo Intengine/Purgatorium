986f9fd3cce57405f8acd6b624ffa5b56a9c8da4
Intengine <studio@intengine.pl>
2019-05-31 16:24:50 +0200

    Trying to set the enemy health and skill damage
    As in description.

---
+++ Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float health = 100f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FX Scripts/SkillDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillDamage : MonoBehaviour
+{
+    public LayerMask enemyLayer;
+    public float radius = 0.5f;
+    public float damageCount = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

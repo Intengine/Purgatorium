b99ce1156e86a9b544e0ed3a365dcd58f29213a2
Intengine <studio@intengine.pl>
2019-06-05 18:43:32 +0200

    Tested taking damage from the player
    Apparently it seems to work...

--- Assets/Scripts/FX Scripts/SkillDamage.cs
+++ Assets/Scripts/FX Scripts/SkillDamage.cs
@@ -9,10 +9,21 @@ public class SkillDamage : MonoBehaviour
     public float damageCount = 10f;
 
     private EnemyHealth enemyHealth;
-    private bool colided;
+    private bool collided;
 
     void Update()
     {
         Collider[] hits = Physics.OverlapSphere(transform.position, radius, enemyLayer);
+
+        foreach(Collider c in hits)
+        {
+            enemyHealth = c.gameObject.GetComponent<EnemyHealth>();
+            collided = true;
+        }
+        if(collided)
+        {
+            enemyHealth.TakeDamage(damageCount);
+            enabled = false;
+        }
     }
 }

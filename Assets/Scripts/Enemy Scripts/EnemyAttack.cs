e75cc01b342f31c4f4edb821b5e6a61798baf247
Intengine <studio@intengine.pl>
2019-06-13 17:55:13 +0200

    Tested enemy's attack - working like a charm
    As in description.

--- Assets/Scripts/Enemy Scripts/EnemyAttack.cs
+++ Assets/Scripts/Enemy Scripts/EnemyAttack.cs
@@ -11,17 +11,21 @@ public class EnemyAttack : MonoBehaviour
     private bool finishedAttack = true;
     private float damageDistance = 2f;
 
+    private PlayerHealth playerHealth;
+
     void Awake()
     {
         playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
         animator = GetComponent<Animator>();
+
+        playerHealth = playerTarget.GetComponent<PlayerHealth>();
     }
 
     void Update()
     {
         if(finishedAttack)
         {
-            // deal damage
+            DealDamage(CheckIfAttacking());
         }
         else
         {
@@ -32,8 +36,30 @@ public class EnemyAttack : MonoBehaviour
         }
     }
 
-    void DealDamage()
+    bool CheckIfAttacking()
     {
+        bool isAttacking = false;
 
+        if(!animator.IsInTransition(0) && animator.GetCurrentAnimatorStateInfo(0).IsName("Atk1") || animator.GetCurrentAnimatorStateInfo(0).IsName("Atk2"))
+        {
+            if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.5f)
+            {
+                isAttacking = true;
+                finishedAttack = false;
+            }
+        }
+
+        return isAttacking;
+    }
+
+    void DealDamage(bool isAttacking)
+    {
+        if(isAttacking)
+        {
+            if(Vector3.Distance(transform.position, playerTarget.position) <= damageDistance)
+            {
+                playerHealth.TakeDamage(damageAmount);
+            }
+        }
     }
 }

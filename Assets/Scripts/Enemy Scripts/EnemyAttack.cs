c713af7da56ac3d853b7edcc1845444e6f42c767
Intengine <studio@intengine.pl>
2019-06-13 17:40:52 +0200

    Started to program enemy's attack
    As in description.

---
+++ Assets/Scripts/Enemy Scripts/EnemyAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float damageAmount = 10f;
+
+    private Transform playerTarget;
+    private Animator animator;
+    private bool finishedAttack = true;
+    private float damageDistance = 2f;
+
+    void Awake()
+    {
+        playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(finishedAttack)
+        {
+            // deal damage
+        }
+        else
+        {
+            if(!animator.IsInTransition(0) && animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            {
+                finishedAttack = true;
+            }
+        }
+    }
+
+    void DealDamage()
+    {
+
+    }
+}

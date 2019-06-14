57696dcffdd1e117c4c053d907966578d2d671eb
Intengine <studio@intengine.pl>
2019-06-14 16:19:28 +0200

    Tested enemy's health
    It's hard to kill the enemy. Will fix that later, but everything seems to work.

--- Assets/Scripts/Enemy Scripts/EnemyHealth.cs
+++ Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     public float health = 100f;
 
+    private Image healthImage;
+
+    void Awake()
+    {
+        if(tag == "Boss")
+        {
+            healthImage = GameObject.Find("Health Foreground Boss").GetComponent<Image>();
+        }
+        else
+        {
+            healthImage = GameObject.Find("Health Foreground").GetComponent<Image>();
+        }
+    }
+
     public void TakeDamage(float amount)
     {
         health -= amount;
 
-        print("Enemy took damage, health is " + health);
+        healthImage.fillAmount = health / 100f;
 
         if(health <= 0)
         {

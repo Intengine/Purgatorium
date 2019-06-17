4b6ddfa62ac71ff13764c0001dd26cf5f96870ec
Intengine <studio@intengine.pl>
2019-06-17 18:45:14 +0200

    Added boss's health
    As in description.

---
+++ Assets/Scripts/Enemy Scripts/BossStateChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BossState
+{
+    NONE,
+    IDLE,
+    PAUSE,
+    ATTACK,
+    DEATH
+}
+
+public class BossStateChecker : MonoBehaviour
+{
+    private Transform playerTarget;
+    private BossState bossState = BossState.NONE;
+    private float distanceToTarget;
+    private EnemyHealth bossHealth;
+
+    void Awake()
+    {
+        playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+        bossHealth = GetComponent<EnemyHealth>();
+    }
+
+    void Update()
+    {
+        
+    }
+}

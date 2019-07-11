9b171b2d73c5b92d566d37e5d5488d98b470974f
Intengine <studio@intengine.pl>
2019-07-11 17:27:25 +0200

    Started creating the boss's movement
    As in description.

---
+++ Assets/Scripts/Enemy Scripts/BossControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossControl : MonoBehaviour
+{
+    private Transform playerTarget;
+    private BossStateChecker bossStateChecker;
+    private NavMeshAgent navAgent;
+    private Animator animator;
+
+    private bool finishedAttacking = true;
+    private float currentAttackTime;
+    private float waitAttackTime = 1f;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

6e09b2d4aee33105cb31dc4305d30e3cabd4e9b3
Intengine <studio@intengine.pl>
2019-07-15 19:50:44 +0200

    Created BossSpecialAttack.cs
    Started to set some special attacks of the boss.

---
+++ Assets/Scripts/Enemy Scripts/BossSpecialAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpecialAttack : MonoBehaviour
+{
+    public GameObject bossFire;
+    public GameObject bossMagic;
+
+    private Transform playerTarget;
+
+    void Awake()
+    {
+        playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void BossFireTornado()
+    {
+        
+    }
+
+    void BossSpell()
+    {
+
+    }
+}
